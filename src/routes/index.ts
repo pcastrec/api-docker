436a4790debe4780db164970375e7b2dde9b8c03
Lenovo <p.castrec@gmail.com>
2023-12-22 12:28:14 +0100

    correction tests

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,22 +1,11 @@
 import express, { Request, Response } from "express";
-<<<<<<< HEAD
-import loginRouter from "./login";
-import thomasRouter from "./thomas.routes";
-=======
 import { users } from "./users.routes";
->>>>>>> daf2541 (correction tests)
 
 const router = express.Router();
 
 router.get('/', (req: Request, res: Response) => {
     res.status(200).json({ message: 'API endpoint reached!' });
 });
-
-<<<<<<< HEAD
-router.use('/api', loginRouter);
-router.use('/thomas', thomasRouter);
-=======
 router.use('/users', users);
->>>>>>> daf2541 (correction tests)
 
 export default router;
